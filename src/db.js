b3916b07023431f9a0bf9461a0693e79b050a0c4
JarneDM <jarne.demeyer05@hotmail.com>
2025-09-12 20:46:36 +0200

    add seeding db when db is empty or new on render

--- src/db.js
+++ src/db.js
@@ -4,5 +4,19 @@ export const db = new Dexie("BoardlyDB");
 db.version(1).stores({
   projects: "++id, name",
   tasks: "++id, title, description, status, labels, projectId, [status+projectId]",
-    labels: "++id, name",
+  labels: "++id, name",
 });
+
+db.on("populate", () => {
+  db.projects.bulkPut([{ name: "My Project" }, { name: "Work" }]);
+  db.labels.bulkPut([
+    { name: "Urgent" },
+    { name: "Low Priority" },
+    { name: "Bug" },
+    { name: "Feature" },
+    { name: "School" },
+    { name: "Personal" },
+  ]);
+});
+
+db.open();
